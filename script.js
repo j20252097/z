d598a2bf6ac9caf9c4b2c1927ec55fbb14147f54
j20252097 <j20252097@gmail.com>
2025-10-18 21:42:28 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -61,7 +61,15 @@ const snd = {
 	lose: new Audio('https://actions.google.com/sounds/v1/cartoon/wood_plank_flicks.ogg')
 };
 
-function playSound(name){ if(!soundEnabled) return; try{ snd[name]?.currentTime=0; snd[name]?.play(); }catch(e){} }
+function playSound(name){
+	if(!soundEnabled) return;
+	try{
+		const s = snd[name];
+		if(!s) return;
+		s.currentTime = 0;
+		s.play();
+	}catch(e){}
+}
 
 function createDeck(){
 	const suits = ['♠','♥','♦','♣'];
